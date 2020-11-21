ccaff4cb76e71f9e9da13f7668ee915466c23d3a
Greg Hawthorne <ghawthorne@me.com>
2020-11-21 08:28:42 +0100

    create static snake position

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("pythonProject4")
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+starting_position = [(0, 0), (-20, 0), (-40, 0)]
+
+for position in starting_position:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
+
+
+screen.exitonclick()
