d148bed62484b26cb4d182a90868363d8b244554
Greg Hawthorne <ghawthorne@me.com>
2020-11-21 09:23:01 +0100

    create class and move snake

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from turtle import Screen, Turtle
+from snake import Snake
+import time
 
 screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
-starting_position = [(0, 0), (-20, 0), (-40, 0)]
+snake = Snake()
 
-for position in starting_position:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.goto(position)
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
 
+    snake.move()
 
 screen.exitonclick()
